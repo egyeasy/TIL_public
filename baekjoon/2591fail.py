f81ab50b4c5174d6a921caa36271ba08bb3a403e
egyeasy <dz1120@gmail.com>
2019-03-07 18:10:49 +0900

    baekjoon

--- baekjoon/2591fail.py
+++ baekjoon/2591fail.py
@@ -1,8 +1,8 @@
 import sys
 sys.stdin = open('2591.txt', 'r')
-
+import time
 from collections import deque
-
+s = time.time()
 def bfs(s):
     global total_cnt
     dq.append(s)
@@ -11,17 +11,17 @@ def bfs(s):
         # if not visited[s]:
         #     visited[s] = 1
         # print("visited", s)
-        if s + 1 >= len_num - 1:
+        if s == len_num - 1:
             total_cnt += 1
-            print("total", total_cnt, "s", s, "i", i)
+            # print("total", total_cnt, "s", s, "i", i)
             continue
-        for i in range(1, 3):
-            if s + i < len_num - 1:
+        for i in [1, 2]:
+            if s + i <= len_num - 1:
                 if i == 1:
-                    print("한글자", "s", s, "i", i)
+                    # print("한글자", "s", s, "i", i)
                     dq.append(s + i)
                 elif num[s + 1] * 10 + num[s + 2] <= 34:
-                    print("두글자", "s", s, "i", i, "값", num[s + 1] * 10 + num[s + 2])
+                    # print("두글자", "s", s, "i", i, "값", num[s + 1] * 10 + num[s + 2])
                     dq.append(s + i)
 
 
@@ -33,3 +33,4 @@ visited = [0] * len(num)
 total_cnt = 0
 bfs(-1)
 print(total_cnt)
+print(time.time() - s)
