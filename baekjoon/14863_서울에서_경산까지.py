952cf8a1de7b884bb7e489cea1a2241d1b724576
egyeasy <dz1120@gmail.com>
2019-03-26 19:58:57 +0900

    baekjoon

---
+++ baekjoon/14863_서울에서_경산까지.py
@@ -0,0 +1,39 @@
+"""
+
+
+> input
+
+
+> output
+
+
+"""
+import sys
+sys.stdin = open('14863.txt', 'r')
+
+N_cities, K_time = map(int, input().split())
+values = [[[0] * 2 for __ in range(2)] for _ in range(N_cities)]
+# print(values)
+for i in range(N_cities):
+    dobo_time, dobo_won, jajun_time, jajun_won = map(int, input().split())
+    values[i][0] = [dobo_time, dobo_won]
+    values[i][1] = [jajun_time, jajun_won]
+
+print(values)
+
+# permutation
+for i in range(1 << N_cities):
+    curr_time = 0
+    curr_won = 0
+    for j in range(N_cities):
+        if i & (1 << j):
+            # 1일 때 처리(자전거)
+            curr_time += values[j][1][0]
+            curr_won += values[j][1][1]
+        else:
+            # 0일 때 처리(도보)
+    
+        # 시간이나 돈 넘칠 때 가지치기
+
+# idea
+# 1.
