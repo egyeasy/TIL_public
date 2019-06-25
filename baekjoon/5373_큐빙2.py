c8f139549581287e7dccbd155957d02e8767c926
egyeasy <dz1120@gmail.com>
2019-06-25 17:20:08 +0900

    baekjoon

--- baekjoon/5373_큐빙2.py
+++ baekjoon/5373_큐빙2.py
@@ -18,50 +18,32 @@ back = face([['o'] * 3 for _ in range(3)], [0, 'r'], [1, 'r'], [5, 'r'], [4, 'l'
 left = face([['g'] * 3 for _ in range(3)], [0, 'l'], [1, 'l'], [3, 'r'], [2, 'l'])
 right = face([['b'] * 3 for _ in range(3)], [0, 'r'], [1, 'r'], [2, 'r'], [3, 'l'])
 
-
-def judge_change(comm):
-    which_side = -1
-    is_plus = -1
-    if comm[1] == '+':
-        is_plus = True
-    else:
-        is_plus = False
-    comm_side = comm[0]
-    for side_idx in range(6):
-        if comm_side == sides[side_idx]:
-            which_side = side_idx
-            return which_side, is_plus
+the_cubic = {'U': up, 'D': down, 'F': front, 'B': back, 'L': left, 'R': right}
 
 
-def rotate_this_side(side_num, is_plus):
+def rotate_the_face(the_face, is_plus):
     new_side = [[0] * 3 for _ in range(3)]
     if is_plus:
         for i in range(3):
             for j in range(3):
-                new_side[j][2 - i] = current_cubic[side_num][i][j]
+                new_side[j][2 - i] = the_cubic[the_face][i][j]
     else:
         for i in range(3):
             for j in range(3):
-                new_side[2 - j][i] = current_cubic[side_num][i][j]
+                new_side[2 - j][i] = the_cubic[the_face][i][j]
     for i in range(3):
         for j in range(3):
-            current_cubic[side_num][i][j] = new_side[i][j]
+            the_cubic[the_face][i][j] = new_side[i][j]
 
 
-def do_change(side_num, is_plus):
-    # 해당 면을 회전시킴
-    rotate_this_side(side_num, is_plus)
-    # 테두리 처리
-    former_edge = [-1, -1, -1]
-    this_edge = [-1, -1, -1]
-    if is_plus:
-        if side_num == 0:
-            pass
-        elif side_num == 2:
-            pass
-            
+def do_change(the_face, sign):
+    if sign == '+':
+        is_plus = True
     else:
-        pass
+        is_plus = False
+    rotate_the_face(the_face, is_plus)
+
+
 
 T = int(input())
 for tc in range(T):
@@ -85,7 +67,6 @@ for tc in range(T):
     print(command_list)
     
     for command in command_list:
-        side_num, is_plus = judge_change(command)
-        do_change(side_num, is_plus)
+        do_change(command[0], command[1])
 
     print()
