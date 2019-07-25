e3f4ab010446f18107f48313be9a67a8bc1538e6
임동영 <dz1120@gmail.com>
2019-07-25 19:06:06 +0900

    bakjoon 1929

--- baekjoon/1929_소수_구하기.py
+++ baekjoon/1929_소수_구하기.py
@@ -3,15 +3,16 @@ sys.stdin = open('1929.txt', 'r')
 
 M, N = map(int, input().split())
 
-# 홀수로 만들기
+# 에라토스테네스의 체 초기화: n개 요소에 True 설정(소수로 간주)
+sieve = [True] * N
+sqrt_N = int(N ** 0.5)
 
-for num in range(M, N + 1):
-    if num == 1:
-        continue
-    is_prime = True
-    for div_num in range(2, num):
-        if num % div_num == 0:
-            is_prime = False
-            break
-    if is_prime:
-        print(num)
+result = []
+
+# n의 최대 약수가 sqrt(n) 이하이므로 sqrt(n)까지 검사
+for num in range(2, sqrt_N  + 1):
+    if sieve[num]:
+        for j in range(num + num, N, num):
+            pass
+    
+        
