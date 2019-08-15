122c3995cfa55a45dea8f4d8bb4bce1cd4a8477c
dz1120 <dz1120@gmail.com>
2019-08-15 10:23:50 +0900

    [doing] baekjoon 10989 수 정렬하기 3

--- baekjoon/10989_수_정렬하기_3.py
+++ baekjoon/10989_수_정렬하기_3.py
@@ -2,14 +2,32 @@ import sys
 sys.stdin = open('10989.txt', 'r')
 
 N = int(input())
-counts = [0] * 10000
-
-for _ in range(N):
-    num = int(input())
-    counts[num - 1] += 1
+counts = [0] * 10001
+inputs = [-1] * N
+outputs = [-1] * N
 
 for i in range(N):
-    count = counts[i]
-    if count:
-        for _ in range(count):
-            print(i + 1)
+    num = int(input())
+    inputs[i] = num
+    counts[num] += 1
+
+# update counts
+for i in range(1, 10000):
+    counts[i + 1] += counts[i]
+
+
+for j in reversed(range(N)):
+    outputs[counts[inputs[j]] - 1] = inputs[j]
+    counts[inputs[j]] -= 1
+
+print('\n'.join(map(str, outputs)))
+
+
+
+
+
+# for i in range(N):
+#     count = counts[i]
+#     if count:
+#         for _ in range(count):
+#             print(i + 1)
