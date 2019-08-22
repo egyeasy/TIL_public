2d252a4a42e226b258c7b865e4147c74b116dbcd
egyeasy <dz1120@gmail.com>
2019-08-22 15:46:23 +0900

    Yoon C++ problem

---
+++ Yoon_C++/1-6/functions.cpp
@@ -0,0 +1,26 @@
+// #include <iostream>
+
+#include "functions.h"
+using namespace std;
+
+void createAccount(int* accountId) {
+    cout << "[계좌개설]" << endl;
+    *accountId += 1;
+    
+}
+
+void send() {
+
+}
+
+void drawOut() {
+
+}
+
+void totalInfo() {
+
+}
+
+void exit() {
+    
+}
---
+++ Yoon_C++/1-6/functions.h
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+void createAccount(int* accountId);
+
+void send();
+
+void drawOut();
+
+void totalInfo();
+
+void exit();
