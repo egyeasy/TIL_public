681de87eee4433a0ba7dd6cea41ce43ff77daca4
egyeasy <dz1120@gmail.com>
2019-08-26 12:46:03 +0900

    [done] baekjoon 1003 피보나치 함수

--- baekjoon/1003_피보나치_함수.py
+++ baekjoon/1003_피보나치_함수.py
@@ -4,42 +4,24 @@ sys.stdin = open('1003.txt', 'r')
 
 zero_cnt = -1
 one_cnt = -1
-
 result = [-1] * 41
+result[0] = (1, 0)
+result[1] = (0, 1)
 
 def fibonacci(n):
     global zero_cnt, one_cnt
-    if n == 0:
-        # print("0")
-        result[n] = (0, 1, 0) # 값, 0횟수, 1횟수
-        zero_cnt += 1
-        # return 0
-    elif n == 1:
-        # print("1")
-        result[n] = (1, 0, 1)
-        one_cnt += 1
-        # return 1
+    if n == 0 or n == 1:
+        return
     else:
-        # if result != -1:
-        #     zero_cnt += result[n][0]
-        #     one_cnt += result[n][1]
-        #     # return result[n][0]
-        # else:
-        result[n] = [result[n - 1][j] + result[n - 2][j] for j in range(3)]
-        print("result n: ", result[n])
-        # print(result)
-        zero_cnt += result[n][1]
-        one_cnt += result[n][2]
-        # if n == 2:
-        #     print(result)
-        # return result[n - 1] + result[n - 2]
-
+        for i in range(2, n + 1):
+            this_zero_cnt = result[i - 1][0] + result[i - 2][0]
+            this_one_cnt = result[i - 1][1] + result[i - 2][1]
+            result[i] = (this_zero_cnt, this_one_cnt)
 
 N = int(sys.stdin.readline())
 for _ in range(N):
     n = int(sys.stdin.readline())
     zero_cnt = 0
     one_cnt = 0
-    for i in range(n + 1):
-        fibonacci(i)
-    print(zero_cnt, one_cnt)
+    fibonacci(n)
+    print(result[n][0], result[n][1])
