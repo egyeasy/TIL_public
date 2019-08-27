619aa28c246c0a29b834faa77f110482a7e547b1
egyeasy <dz1120@gmail.com>
2019-08-27 13:30:54 +0900

    [doing] baekjoon 1904 01타일

---
+++ baekjoon/1904_01타일.py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin = open('1904.txt', 'r')
+
+
+def fibonacci(n):
+    for i in range(3, n + 1):
+        results[i] = results[i - 1] + results[i - 2]
+    return results[n]
+
+N = int(input())
+
+results = [0] * (N + 1)
+results[1] = 1
+results[2] = 2
+
+result = fibonacci(N)
+
+# print(results)
+print(result % 15746)
