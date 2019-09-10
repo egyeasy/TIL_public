c690286ea72515f089882acc88bba781fc9f2d7b
egyeasy <dz1120@gmail.com>
2019-09-10 19:13:49 +0900

    baekjoon 2217 로프

--- baekjoon/2217_로프.py
+++ baekjoon/2217_로프.py
@@ -2,11 +2,20 @@ import sys
 sys.stdin = open('2217.txt', 'r')
 
 N = int(input())
-min_weight = 10001
+weights = []
 for i in range(N):
     weight = int(sys.stdin.readline())
-    if weight < min_weight:
-        min_weight = weight
+    weights.append(weight)
 
-answer = min_weight * N
-print(answer)
+weights.sort(reverse=True)
+total_max = 0
+
+for cnt, weight in enumerate(weights):
+    curr_max = (cnt + 1) * weight
+    if curr_max > total_max:
+        total_max = curr_max
+    # 아래 문을 넣으면 반례 존재
+    # else:
+    #     break
+
+print(total_max)
