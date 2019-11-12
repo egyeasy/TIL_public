f74040c3bee65bfb760ec6547ac07ac231ebc9d4
egyeasy <dz1120@gmail.com>
2019-11-12 14:23:15 +0900

    [Doing] baekjoon 11066 파일 합치기

--- baekjoon/11066_파일_합치기.py
+++ baekjoon/11066_파일_합치기.py
@@ -3,3 +3,24 @@
 import sys
 sys.stdin = open('11066.txt', 'r')
 
+TC = int(input())
+for tc in range(TC):
+    K = int(input())
+    nums = list(map(int, input().split()))
+    nums.sort()
+    print(nums)
+    
+    total_cost = 0
+
+    while len(nums) > 1:
+        print(nums)
+        added_cost = nums[0] + nums[1]
+        total_cost += added_cost
+        nums[0] = added_cost
+        nums.pop(1)
+        nums.sort()
+        print("added: ", added_cost)
+        print("total: ", total_cost)
+    
+    print(total_cost)
+        
