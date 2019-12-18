eb294cb4c940decae9a3d2808da0e56dc2f840fa
egyeasy <dz1120@gmail.com>
2019-12-18 23:56:43 +0900

    Hong c++ Chapter8-8

--- HongC++/HongChapter8/Chapter8_8 클래스 코드와 헤더 파일/Calc.cpp
+++ HongC++/HongChapter8/Chapter8_8 클래스 코드와 헤더 파일/Calc.cpp
@@ -1,7 +1,7 @@
 #include "Calc.h"
 
 
-Calc& Calc::add(int value) { m_value += value; return *this; }
+Calc& Calc::add(int value) {m_value += value; return *this; }
 Calc& Calc::sub(int value) { m_value -= value; return *this; }
 Calc& Calc::mult(int value) { m_value *= value; return *this; }
 
--- SWEA/[SW Test 샘플문제]1767_프로세서_연결하기.cpp
+++ SWEA/[SW Test 샘플문제]1767_프로세서_연결하기.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
-
+#include <cstdio>
 using namespace std;
 
 int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+    //ios::sync_with_stdio(0);
+    //cin.tie(0);
 
     int TC;
+    int N_side;
+    int matrix[12][12];
 
     for (int tc = 1; tc <= TC; tc++)
     {
-        int N_side;
         cin >> N_side;
+        
 
-        int 
+        
     }
 
     return 0;
