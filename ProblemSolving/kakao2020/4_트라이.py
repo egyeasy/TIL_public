65f2b21f563c2ec6ecac3d74aa6a5d760e4b6a4b
egyeasy <dz1120@gmail.com>
2020-08-07 21:34:12 +0900

    kakao2020 4번 동훈 풀이 보고 품

--- ProblemSolving/kakao2020/4_트라이.py
+++ ProblemSolving/kakao2020/4_트라이.py
@@ -1,102 +1,50 @@
-# 2020.08.05
+# 2020.08.06 동훈 솔루션 보고 다시
 
-# 트라이 구현 참고
-# https://blog.ilkyu.kr/entry/%ED%8C%8C%EC%9D%B4%EC%8D%AC%EC%97%90%EC%84%9C-Trie-%ED%8A%B8%EB%9D%BC%EC%9D%B4-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0
-
-from collections import deque
-
-class Node(object):
-
-    def __init__(self, key, data=None):
-        self.key = key
-        self.data = data
+class Node:
+    def __init__(self, char):
+        self.char = char
+        self.count = 0
         self.children = {}
 
-class Trie(object):
+class Trie:
     def __init__(self):
         self.head = Node(None)
 
-    def insert(self, string):
+    def insert(self, word):
         curr_node = self.head
-
-        for char in string:
-            if char not in curr_node.children:
-                curr_node.children[char] = Node(char)
-            
-            curr_node = curr_node.children[char]
-            # string의 마지막 글자 차례이면,
-            # 노드의 data 필드에 저장하려는 문자열 전체를 저장한다.
-        curr_node.data = string
-
-    """
-    주어진 단어 string이 트라이에 존재하는지 여부를 반환합니다.
-    """
-    def search(self, string):
-        curr_node = self.head
-
-        for char in string:
-            if char in curr_node.children:
-                curr_node = curr_node.children[char]
+        for c in word:
+            if c in curr_node.children.keys():
+                curr_node = curr_node.children[c]
             else:
-                return False
-        
-        # string의 마지막 글자에 다다랐을 때,
-        # curr_node에 data가 있다면 string이 트라이에 존재하는 것!
-        if (curr_node.data != None):
-            return True
-        
-    """
-    주어진 prefix로 시작하는 단어들을
-    트라이에서 찾아 리스트 형태로 반환합니다.
-    """
-    def starts_with(self, prefix, len_word):
+                curr_node.children[c] = Node(c)
+                curr_node = curr_node.children[c]
+            curr_node.count += 1
+    
+    def search(self, prefix):
         curr_node = self.head
-        result = 0
-        subtrie = curr_node
-
-        # 트라이에서 prefix를 찾고,
-        # prefix의 마지막 글자 노드를 subtrie로 설정
-        for char in prefix:
-            if char in curr_node.children:
-                curr_node = curr_node.children[char]
-                subtrie = curr_node
-            else:
-                return result
+        for c in prefix:
+            if not c in curr_node.children.keys():
+                return 0
+            curr_node = curr_node.children[c]
+        return curr_node.count
         
-        # bfs로 prefix subtrie를 순회하며
-        # data가 있는 노드들(=완전한 단어)를 찾는다.
-        queue = deque(subtrie.children.values())
-
-        while queue:
-            curr = queue.popleft()
-            if curr.data != None and len(curr.data) == len_word:
-                result += 1
-
-            queue += deque(curr.children.values())
-        
-        return result
-
-
-
+    
 def solution(words, queries):
-    len_query = len(queries)
-    answer = [0] * len_query
-    trie = Trie()
-    reversed_trie = Trie()
-
+    tries = [Trie() for _ in range(10001)]
+    r_tries = [Trie() for _ in range(10001)]
+    answer = [0] * len(queries)
     for word in words:
-        trie.insert(word)
-        reversed_trie.insert(word[::-1])
-
-    for i in range(len_query):
-        query = queries[i]
+        tries[len(word)].insert(word)
+        r_tries[len(word)].insert(word[::-1])
+    
+    for i, query in enumerate(queries):
         if query[-1] == "?":
-            answer[i] = trie.starts_with(query.split("?")[0], len(query))
+            answer[i] = tries[len(query)].search(query.split("?")[0])
         else:
-            reversed_query = query[::-1]
-            answer[i] = reversed_trie.starts_with(reversed_query.split("?")[0], len(query))
-        
+            answer[i] = r_tries[len(query)].search(query.split("?")[-1][::-1])
     return answer
 
-print(solution(["frodo", "front", "frost", "frozen", "frame", "kakao"],
-                ["fro??", "????o", "fr???", "fro???", "pro?"]))
+print(solution(["frodo", "front", "frost", "frozen", "frame", "kakao"], ["fro??", "????o", "fr???", "fro???", "pro?"]))
+print(solution(["abcc", "a", "ab", "abccd"], ["?"]))
+print(solution(["fff", "fer", "ffef", "fsrrere"], ["?", "???", "f??", "??????e", "?????????", "efkejflewjfe?", "????fejfoiewfj"]))
+print(solution(["f", "fr"], ["?", "f?"]))
